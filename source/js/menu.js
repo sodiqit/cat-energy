0c5be6b3d934bb57c7267be42de5382ada518ff0
Даниил <daniil.petro3@gmail.com>
2019-05-17 23:53:33 +0500

    Адаптировал секцию h1

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,5 +1,6 @@
 var button = document.querySelector(".main_menu__button");
 var menu = document.querySelector(".main_menu");
+var link = document.querySelector(".main_menu__link");
 
 menu.classList.remove("main_menu--no-js");
 
@@ -7,4 +8,12 @@ button.addEventListener("click", function (evt) {
   evt.preventDefault();
   button.classList.toggle("main_menu__button--active");
   menu.classList.toggle("main_menu--js");
+});
+
+link.addEventListener("click", function (evt) {
+  if (link.classList.contains("main_menu__link--logo")) {
+    link.classList.remove("main_menu__link--active");
+  } else {
+    link.classList.toggle("main_menu__link--active");
+  }
 });
